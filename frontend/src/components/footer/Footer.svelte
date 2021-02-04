8daae7915baa33d790cbbc207845fcec06c7d6fe
Patrick Hener <patrick.hener@syss.de>
2021-02-04 08:41:30 +0100

    changed hyperlink

--- frontend/src/components/footer/Footer.svelte
+++ frontend/src/components/footer/Footer.svelte
@@ -2,18 +2,18 @@
 </script>
 
 <footer class="grid grid-cols-3 gap-4 p-4 w-full border-t border-white pin-b">
-  <div class="text-left">
-    <a
-      href="https://dascr.org"
-      target="_blank"
-      rel="noopener">https://dascr.org</a>
-  </div>
-  <div class="text-center"><b>DaSCR - score and more</b></div>
-  <div class="text-right">
-    <a
-      href="https://github.com/dascr/board"
-      target="_blank"
-      rel="noopener"
-      class="font-bold"><i class="fab fa-github fa-2x" /></a>
-  </div>
+    <div class="text-left">
+        <a
+            href="https://dascr.org"
+            target="_blank"
+            rel="noopener">https://dascr.org</a>
+    </div>
+    <div class="text-center"><b>DaSCR - score and more</b></div>
+    <div class="text-right">
+        <a
+            href="https://github.com/dascr/dascr-board"
+            target="_blank"
+            rel="noopener"
+            class="font-bold"><i class="fab fa-github fa-2x" /></a>
+    </div>
 </footer>
