cb63e54c465716a151bff6c04b0e3e887e6528b4
Patrick Hener <patrick.hener@to.com>
2021-08-29 15:57:55 +0200

    build

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -5,10 +5,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
   kit: {
     adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
+      fallback: 'index.html',
     }),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
